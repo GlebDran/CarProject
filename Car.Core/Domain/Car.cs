8e2ca5aba9aa451a89c79f2e5b46a40f6792b86a
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2026-01-13 14:59:56 +0200

    Added Car domain entity

---
+++ Car.Core/Domain/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car.Core.Domain
+{
+    public class Car
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Make { get; set; } // Производитель (BMW, Audi...)
+        public string Model { get; set; } // Модель
+        public int Year { get; set; } // Год
+        public int Price { get; set; } // Цена
+        public bool IsUsed { get; set; } // Б/у или новая (для количества переменных)
+
+        // Обязательные поля по заданию
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
